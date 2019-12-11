cdc4a5ab641abe2d16353a7a8f36011cccb3e4ee
User1 <User1@RDV-005604>
2019-12-11 17:40:38 -0500

    trying to fix things

---
+++ Final1211/FinalProject/Grublin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class Grublin : Monster
+    {
+
+
+        public Grublin(int health, bool hasweapon, int damage)
+        {
+            this.Health = health;
+            this.HasWeapon = hasweapon;
+            this.Damage = damage;
+        }
+
+        
+        
+
+        
+    }
+}
---
+++ Final1211/FinalProject/Monster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class Monster
+    {
+        public int Health
+        {
+            get;
+
+            set;
+        }
+        public bool HasWeapon
+        {
+            get;
+
+            set;
+        }
+        public int Damage
+        {
+            get;
+
+            set;
+        }
+        public void MonsterAttack(ref Player player)
+        {
+            player.PlayerHealth -= Damage;
+        }
+
+    }
+}
